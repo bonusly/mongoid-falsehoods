3fe0d5707cd6be8445e5b0853f9fdbea2e538326
Jon Evans <joncodes+github@gmail.com>
2020-04-11 14:58:13 -0600

    Add specs

--- spec/mongoid/falsehoods_spec.rb
+++ spec/mongoid/falsehoods_spec.rb
@@ -1,5 +1,23 @@
 RSpec.describe Mongoid::Falsehoods do
-  it "has a version number" do
-    expect(Mongoid::Falsehoods::VERSION).not_to be nil
+  context 'not breaking DateTime' do
+    let(:date_time) { DateTime.new(2020, 2, 21, 10, 0, 0) }
+
+    it 'mongoizes' do
+      expect(DateTime.mongoize(date_time)).to eq date_time
+    end
+
+    it 'demongoizes' do
+      expect(DateTime.demongoize(date_time)).to eq date_time
+    end
+  end
+
+  context 'keeping false as false instead of nil' do
+    it 'mongoizes' do
+      expect(DateTime.mongoize(false)).to eq false
+    end
+
+    it 'demongoizes' do
+      expect(DateTime.demongoize(false)).to eq false
+    end
   end
 end
